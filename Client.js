e4040ad527632232f2f3d16644f8cea11dfbf324
Aurélien <lebreton97480@gmail.com>
2025-04-18 19:55:40 +0400

    Update

--- Client.js
+++ Client.js
@@ -1,3 +1,261 @@
+// Global variables
+
+let _DeltaTime_;
+
+let LocalID = -1,
+  client;
+
+//const ip = "ws://game-netcode-exemple-production.up.railway.app";
+//const port = 8080;
+
+const REQ_ConnexionJOIN = 0,
+  REQ_PlayerJOIN = 1,
+  REQ_PlayerSYNC = 2,
+  REQ_PlayerLEAVE = 3,
+  REQ_PlayerInputPayload = 4,
+  REQ_PlayerStatePayload = 5;
+
+//C'est la list des joueurs
+const objects = [];
+
+// Gestion des input clavier et souris
+class Input {
+  constructor() {
+    this.keys = new Set();
+
+    this.mouse = {
+      x: 0,
+      y: 0,
+      down: false,
+    };
+
+    window.addEventListener("keydown", (event) => {
+      this.keys.add(event.key);
+    });
+
+    window.addEventListener("keyup", (event) => {
+      this.keys.delete(event.key);
+    });
+
+    window.addEventListener("mousemove", (event) => {
+      this.mouse.x = event.clientX;
+      this.mouse.y = event.clientY;
+    });
+
+    window.addEventListener("mousedown", () => {
+      this.mouse.down = true;
+    });
+
+    window.addEventListener("mouseup", () => {
+      this.mouse.down = false;
+    });
+  }
+
+  isKeyPressed(key) {
+    return this.keys.has(key);
+  }
+
+  isMouseDown() {
+    return this.mouse.down;
+  }
+
+  getMousePosition() {
+    return {
+      x: this.mouse.x,
+      y: this.mouse.y,
+    };
+  }
+}
+
+// Payload class
+class Payload {
+  constructor(value, tick) {
+    this.value = value;
+    this.tick = tick;
+  }
+
+  get Value() {
+    return this.value;
+  }
+
+  set Value(value) {
+    this.value = value;
+  }
+
+  get Tick() {
+    return this.tick;
+  }
+
+  set Tick(tick) {
+    this.tick = tick;
+  }
+
+}
+
+
+const BUFFER_SIZE = 1024;
+const SERVER_TICK_RATE = 30;
+const minTimeBetweenTicks = 1 / SERVER_TICK_RATE;
+
+// Player class
+class Player {
+
+  constructor(id, pos) {
+    this.input = new Input();
+    this.id = id;
+
+    this.dir = {
+      x: 0,
+      y: 0,
+    }
+
+    this.pos = pos;
+    this.speed = 150;
+
+    this.timer = 0;
+    this.currentTick = 0;
+
+    this.stateBuffer = []
+    this.inputBuffer = []
+
+    this.latestServerState = null;
+    this.lastProcessedState = null;
+  }
+
+  set LatestServerState(value) {
+    this.latestServerState = value;
+  }
+
+  Distance(a, b) {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+  }
+
+  HandleServerReconciliation() {
+    this.lastProcessedState = this.latestServerState;
+
+    let serverStateBufferIndex = this.latestServerState.tick % BUFFER_SIZE;
+    let positionError = Distance(
+      this.latestServerState.value.position,
+      this.stateBuffer[serverStateBufferIndex].value.position);
+
+    if (positionError > 0.001) {
+      this.pos = latestServerState.value.pos;
+
+      this.stateBuffer[serverStateBufferIndex] = this.latestServerState;
+      let tickToProcess = this.latestServerState.tick + 1;
+
+      while (tickToProcess < this.currentTick) {
+        let bufferIndex = tickToProcess % BUFFER_SIZE;
+
+        // Process new movement with reconciled state
+        let statePayload = ProcessMovement(this.inputBuffer[bufferIndex]);
+
+        // Update buffer with recalculated state
+        this.stateBuffer[bufferIndex] = statePayload;
+
+        tickToProcess++;
+      }
+    }
+  }
+
+  HandleTick() {
+
+    if (!this.latestServerState === null &&
+      (this.lastProcessedState === null ||
+        !this.latestServerState === this.lastProcessedState)) {
+      this.HandleServerReconciliation();
+    }
+
+    if (LocalID != this.id) {
+      return;
+    }
+
+    const bufferIndex = this.currentTick % BUFFER_SIZE;
+
+    const inputPayload = new Payload();
+
+    inputPayload.tick = this.currentTick;
+    inputPayload.value = {
+      dir: this.dir
+    }
+
+    this.inputBuffer[bufferIndex] = inputPayload;
+    this.stateBuffer[bufferIndex] = this.ProcessMovement(inputPayload);
+
+    client.send(JSON.stringify({
+      resquetID: REQ_PlayerInputPayload,
+      data: inputPayload,
+    }));
+
+    //console.log("Sending input", inputPayload, "at tick", this.currentTick);
+
+  }
+
+  ProcessMovement(inputPayload) {
+    this.pos.x += inputPayload.value.dir.x * this.speed * _DeltaTime_;
+    this.pos.y += inputPayload.value.dir.y * this.speed * _DeltaTime_;
+
+    let statePayload = new Payload()
+
+    statePayload.tick = inputPayload.tick;
+    statePayload.value = {
+      pos: this.pos,
+    }
+
+    return statePayload;
+  }
+
+  UpdateInput() {
+
+    if (this.input.isKeyPressed("ArrowUp")) {
+      this.dir.y = -1;
+    }
+    else if (this.input.isKeyPressed("ArrowDown")) {
+      this.dir.y = 1;
+    }
+    else {
+      this.dir.y = 0;
+    }
+
+    if (this.input.isKeyPressed("ArrowLeft")) {
+      this.dir.x = -1;
+    }
+    else if (this.input.isKeyPressed("ArrowRight")) {
+      this.dir.x = 1;
+    }
+    else {
+      this.dir.x = 0;
+    }
+  }
+
+  Update() {
+
+    if (LocalID != this.id) {
+      return;
+    }
+
+    this.UpdateInput();
+
+    this.timer += _DeltaTime_;
+
+    while (this.timer >= minTimeBetweenTicks) {
+      this.timer -= minTimeBetweenTicks;
+      this.HandleTick();
+      this.currentTick++;
+    }
+
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+      canvasCenterX + this.pos.x - (50 / 2),
+      canvasCenterY + this.pos.y - (50 / 2)
+      , 50, 50);
+  }
+
+}
+
 const canvas = document.getElementById("game");
 
 if (!canvas) {
@@ -7,12 +265,18 @@ if (!canvas) {
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const canvasCenterX = canvas.width / 2,
+  canvasCenterY = canvas.height / 2;
+
 const formeConnexion = document.getElementById("connection-form");
 
 formeConnexion.addEventListener("submit", (event) => {
   event.preventDefault();
-  const ip = document.getElementById("ip").value;
-  const port = document.getElementById("port").value;
+
+  // WebSocket connection
+
+  const ip = document.getElementById("IP").value,
+    port = document.getElementById("port").value;
 
   if (!ip || !port) {
     alert("Please enter both IP and port.");
@@ -21,33 +285,23 @@ formeConnexion.addEventListener("submit", (event) => {
 
   console.log("Connecting to server at", ip, "on port", port);
 
-  Start(ip, port);
+  start(ip, port, () => {
+    formeConnexion.style.display = "none";
+  });
 
 })
 
-function Start(ip, port) {
-
-  let LocalID = -1;
-
-  //const ip = "ws://game-netcode-exemple-production.up.railway.app";
-  //const port = 8080;
-
-  const REQ_ConnexionJOIN = 0;
-  const REQ_PlayerJOIN = 1;
-  const REQ_PlayerSYNC = 2;
-  const REQ_PlayerLEAVE = 3;
-
-  //C'est la list des joueurs
-  const objects = [];
-
-  const client = new WebSocket(`${ip}`);
+function start(ip, port, callback) {
+  client = new WebSocket(`${ip}:${port}`);
 
   client.onopen = () => {
     console.log("Connected to the server");
+    callback();
   }
 
   client.onclose = () => {
     console.log("Disconnected from the server");
+    alert("Disconnected from the server");
   }
 
   client.onmessage = (event) => {
@@ -57,8 +311,8 @@ function Start(ip, port) {
       case REQ_ConnexionJOIN:
         LocalID = paquet.data.id;
         const player = new Player(LocalID, {
-          x: centerX - paquet.data.x / 2,
-          y: centerY - paquet.data.y / 2,
+          x: paquet.data.x,
+          y: paquet.data.y,
         });
         objects.push(player);
         break;
@@ -70,14 +324,25 @@ function Start(ip, port) {
         objects.push(newPlayer);
         break;
       case REQ_PlayerSYNC:
+        {
+          const syncPlayer = objects.find((object) => object.id === paquet.data.id);
+          if (syncPlayer) {
+            syncPlayer.pos.x = paquet.data.x;
+            syncPlayer.pos.y = paquet.data.y;
+          }
+          else {
+            console.log("Player not found", paquet.data.id);
+          }
+        }
+        break;
+      case REQ_PlayerInputPayload:
         const syncPlayer = objects.find((object) => object.id === paquet.data.id);
+
         if (syncPlayer) {
-          syncPlayer.pos.x = paquet.data.x;
-          syncPlayer.pos.y = paquet.data.y;
+          //console.log("Player found", paquet.data);
+          syncPlayer.LatestServerState = new Payload(paquet.data.value, paquet.data.tick);
         }
-        else {
-          console.log("Player not found", paquet.data.id);
-        }
+
         break;
       case REQ_PlayerLEAVE:
         const leavePlayerIndex = objects.findIndex((object) => object.id === paquet.data.id);
@@ -90,106 +355,11 @@ function Start(ip, port) {
     }
   }
 
-  class Input {
-    constructor() {
-      this.keys = new Set();
-      this.mouse = {
-        x: 0,
-        y: 0,
-        down: false,
-      };
+  // End WebSocket connection
 
-      window.addEventListener("keydown", (event) => {
-        this.keys.add(event.key);
-      });
+  let lastTime, elapsed;
 
-      window.addEventListener("keyup", (event) => {
-        this.keys.delete(event.key);
-      });
-
-      window.addEventListener("mousemove", (event) => {
-        this.mouse.x = event.clientX;
-        this.mouse.y = event.clientY;
-      });
-
-      window.addEventListener("mousedown", () => {
-        this.mouse.down = true;
-      });
-
-      window.addEventListener("mouseup", () => {
-        this.mouse.down = false;
-      });
-    }
-
-    isKeyPressed(key) {
-      return this.keys.has(key);
-    }
-
-    isMouseDown() {
-      return this.mouse.down;
-    }
-
-    getMousePosition() {
-      return {
-        x: this.mouse.x,
-        y: this.mouse.y,
-      };
-    }
-
-
-  }
-
-  class Player {
-
-    constructor(id, pos) {
-      this.input = new Input();
-      this.id = id;
-      this.pos = pos;
-      this.speed = 20;
-    }
-
-    Update(elapsed) {
-      if (LocalID != this.id) {
-        return;
-      }
-
-      if (this.input.isKeyPressed("ArrowUp")) {
-        this.pos.y -= this.speed * elapsed / 100;
-      }
-      if (this.input.isKeyPressed("ArrowDown")) {
-        this.pos.y += this.speed * elapsed / 100;
-      }
-      if (this.input.isKeyPressed("ArrowLeft")) {
-        this.pos.x -= this.speed * elapsed / 100;
-      }
-      if (this.input.isKeyPressed("ArrowRight")) {
-        this.pos.x += this.speed * elapsed / 100;
-      }
-
-      client.send(JSON.stringify({
-        resquetID: REQ_PlayerSYNC,
-        data: {
-          x: this.pos.x,
-          y: this.pos.y,
-        },
-      }));
-    }
-
-    draw(ctx) {
-      ctx.fillStyle = "red";
-      ctx.fillRect(this.pos.x, this.pos.y, 50, 50);
-    }
-
-  }
-
-  let lastTime;
   const requiredElapsed = 1000 / 30; // 30 FPS
-
-  const centerX = canvas.width / 2;
-  const centerY = canvas.height / 2;
-
-  console.log("Canvas size: ", canvas.width, canvas.height);
-  console.log("Center: ", centerX, centerY);
 
   const ctx = canvas.getContext("2d");
 
@@ -198,7 +368,14 @@ function Start(ip, port) {
     return;
   }
 
-  function HandleTick(elapsed) {
+  function handleLogic() {
+    objects.forEach((object) => {
+      object.Update();
+    });
+  }
+
+  function handleRender() {
+    let fps = Math.floor(1000 / elapsed);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "black";
@@ -207,27 +384,35 @@ function Start(ip, port) {
     ctx.fillStyle = "white";
     ctx.font = "30px Arial";
     ctx.fillText("NetCode V1", 10, 50);
-    ctx.fillText("FPS: " + Math.floor(1000 / elapsed), 10, 100);
-    ctx.fillText("LocalID: " + LocalID, 10, 150);
+    ctx.fillText("FPS: " + fps, 10, 100);
+    ctx.fillText("DeltaTime: " + _DeltaTime_, 10, 150);
+    ctx.fillText("LocalID: " + LocalID, 10, 200);
 
     objects.forEach((object) => {
-      object.Update(elapsed);
       object.draw(ctx);
     });
   }
 
-  function Update(now) {
-    requestAnimationFrame(Update);
+  function update(now) {
+    requestAnimationFrame(update);
+
+    if (LocalID === -1) {
+      return;
+    }
 
     if (!lastTime) { lastTime = now; }
-    let elapsed = now - lastTime;
+
+    elapsed = now - lastTime;
+    _DeltaTime_ = elapsed / 1000;
 
     if (elapsed > requiredElapsed) {
-      HandleTick(elapsed);
+      handleLogic();
+      handleRender();
       lastTime = now;
     }
+
   }
 
-  Update();
+  update();
 
 }
--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const server = require("ws");
 const WebSocket = server.Server;
 
-const port = Number(process.env.PORT)
+const port = Number(process.env.PORT) || 8080;
 
 const wss = new WebSocket({
   port: port,
@@ -12,28 +12,216 @@ const REQ_PlayerJOIN = 1;
 const REQ_PlayerSYNC = 2;
 const REQ_PlayerLEAVE = 3;
 
+const REQ_PlayerInputPayload = 4;
+const REQ_PlayerStatePayload = 5;
+
 const players = [];
+
+class Payload {
+  constructor(value, tick) {
+    this.value = value;
+    this.tick = tick;
+  }
+
+  get Value() {
+    return this.value;
+  }
+
+  set Value(value) {
+    this.value = value;
+  }
+
+  get Tick() {
+    return this.tick;
+  }
+
+  set Tick(tick) {
+    this.tick = tick;
+  }
+
+}
+
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  Enqueue(item) {
+    this.items.push(item);
+  }
+
+  Dequeue() {
+    if (this.items.length === 0) {
+      return null; // ou throw new Error("Queue is empty");
+    }
+    return this.items.shift();
+  }
+
+  Count() {
+    return this.items.length;
+  }
+}
 
 const send = (ws, data) => {
   ws.send(JSON.stringify(data));
 }
 
+
+const SERVER_TICK_RATE = 30;
+const BUFFER_SIZE = 1024;
+const minTimeBetweenTicks = 1 / SERVER_TICK_RATE;
+
+const FRAME_DURATION = 1000 / 30;
+
+let lastTime = Date.now();
+let _DeltaTime_ = 0;
+
+function Loop() {
+  const now = Date.now();
+  _DeltaTime_ = (now - lastTime) / 1000;
+  lastTime = now;
+
+  Update();
+
+  setTimeout(Loop, FRAME_DURATION);
+}
+
+function Update() {
+  players.forEach((player) => {
+    player.Update()
+  })
+  //console.log(`deltaTime: ${dt.toFixed(3)}s`);
+}
+
+Loop();
+
+class Player {
+  constructor(ws, publicId) {
+    this.ws = ws;
+    this.publicId = publicId;
+
+    this.x = Math.random() * 400;
+    this.y = Math.random() * 400;
+    this.speed = 150;
+
+    this.stateBuffer = [];
+    this.timer = 0;
+    this.currentTick = 0;
+
+    this.inputQueue = new Queue();
+  }
+
+  ProcessMovement(input) {
+
+    this.x += input.value.dir.x * this.speed * _DeltaTime_;
+    this.y += input.value.dir.y * this.speed * _DeltaTime_;
+
+    let statePayload = new Payload()
+
+    statePayload.tick = input.tick;
+    statePayload.value = {
+      pos: {
+        x: this.x,
+        y: this.y,
+      },
+    }
+
+    return statePayload;
+
+  }
+
+  HandleTick() {
+
+    players.forEach((player) => {
+      if (player.ws === this.ws) return;
+      send(player.ws, {
+        resquetID: REQ_PlayerSYNC,
+        data: {
+          id: this.publicId,
+          x: this.x,
+          y: this.y,
+        },
+      });
+    });
+
+    let bufferIndex = -1;
+
+    while (this.inputQueue.Count() > 0) {
+      let inputPayload = this.inputQueue.Dequeue();
+
+      bufferIndex = inputPayload.tick % BUFFER_SIZE;
+
+      let statePayload = this.ProcessMovement(inputPayload);
+
+      this.stateBuffer[bufferIndex] = statePayload;
+    }
+
+    if (bufferIndex != -1) {
+
+      let _statePayload = this.stateBuffer[bufferIndex];
+
+      send(this.ws, {
+        resquetID: REQ_PlayerInputPayload,
+        data: {
+          id: this.publicId,
+          tick: _statePayload.tick,
+          value: _statePayload.value
+        },
+      });
+
+      //console.log("Sending state", _statePayload, "at tick", this.currentTick);
+      //envoyer la position du joueur à tous les autres joueurs
+      //players.forEach((player) => {
+      //if (player.ws === this.ws) return;
+
+      //});
+
+    }
+  }
+
+  Update() {
+    this.timer += _DeltaTime_;
+
+    while (this.timer >= minTimeBetweenTicks) {
+      this.timer -= minTimeBetweenTicks;
+      this.HandleTick();
+      this.currentTick++;
+    }
+
+  }
+
+  OnClientInput(inputPayload) {
+    this.inputQueue.Enqueue(inputPayload);
+  }
+}
+
+
 wss.on("connection", (ws) => {
-  const privateId = Math.random().toString(36).substring(2, 9); // generate a random id for the player
+  //const privateId = Math.random().toString(36).substring(2, 9); // generate a random id for the player
   const publicId = Math.random().toString(36).substring(2, 9); // generate a public id for the player
 
-  const new_player = {
+  /*const new_player = {
     ws: ws,
     privateId: privateId,
     publicId: publicId,
     x: Math.random() * 400,
     y: Math.random() * 400,
-  };
+    speed: 150,
+
+    stateBuffer: [],
+    timer: 0,
+    currentTick: 0,
+
+    inputQueue: new Queue(),
+
+  };*/
+
+  const new_player = new Player(ws, publicId);
 
   send(ws, {
     resquetID: REQ_ConnexionJOIN,
     data: {
-      id: privateId,
+      id: publicId,
       x: new_player.x,
       y: new_player.y,
     },
@@ -42,7 +230,7 @@ wss.on("connection", (ws) => {
   players.push(new_player);
 
   players.forEach((player) => {
-    if (player.privateId === privateId) return;
+    if (player.publicId === publicId) return;
 
     // recuperer la position des autres joueurs
     send(ws, {
@@ -67,8 +255,8 @@ wss.on("connection", (ws) => {
 
   ws.on("message", (message) => {
     const paquet = JSON.parse(message);
-
     switch (paquet.resquetID) {
+      /*
       case REQ_PlayerSYNC:
         //mettre à jour la position du joueur
 
@@ -95,13 +283,24 @@ wss.on("connection", (ws) => {
 
         });
 
+        break;*/
+      case REQ_PlayerInputPayload:
+
+        //console.log("Input received", paquet.data);
+
+        let inputPayload = new Payload(
+          paquet.data.value, paquet.data.tick
+        );
+
+        new_player.OnClientInput(inputPayload);
+
         break;
     }
   });
 
   ws.on("close", () => {
     // supprimer le joueur de la liste des joueurs
-    const playerIndex = players.findIndex((player) => player.privateId === privateId);
+    const playerIndex = players.findIndex((player) => player.publicId === publicId);
 
     if (playerIndex !== -1) {
       players.splice(playerIndex, 1);
